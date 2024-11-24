8f94e4a32b5622aba8224e1c398a670e0ebf3b7c
Marat Nazirov <nazimovma@sovcombank.ru>
2024-11-24 15:55:54 +0300

    common init

---
+++ NativeAvatar/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  Created by Marat Nazirov on 24.11.2024.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        commonInit()
+    }
+}
+
+// MARK: - Private methods
+
+private extension ViewController {
+    func commonInit() {
+        view.backgroundColor = .white
+        setupSubviews()
+        setupConstraints()
+    }
+    
+    func setupSubviews() {
+        
+    }
+    
+    func setupConstraints() {
+        
+    }
+}
+
